652cfe34acbac863ffc5a02852fe2a8efe3fb0bf
tommyjr1 <tommyjr1@naver.com>
2021-07-10 18:45:20 +0000

    list modify

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var io = require('socket.io')(server)
 
 var waiters_name = new Array()
 var waiters_id = new Array()
-var rooms = ["1", "2", "3", "4", "5"]
+var rooms = []
 var cards = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
 var nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '+', '-', '*', '/']
 
@@ -25,6 +25,7 @@ io.sockets.on('connection', (socket) => {
 
     waiters_name.push(waiter)
     waiters_id.push(socket.id)
+    console.log(waiters_name, waiters_id)
   })
 
   socket.on('battle', (data) => {
@@ -38,6 +39,8 @@ io.sockets.on('connection', (socket) => {
       var pos = waiters_name.indexOf(accept)
       socket.to(waiters_id[pos]).emit("challengeCome", ask, accept)
     }
+
+    socket.join(ask + "&" + accept)
   })
 
   socket.on('acceptGame', (ask, accept) => {
@@ -46,17 +49,31 @@ io.sockets.on('connection', (socket) => {
     var pos_ask = waiters_name.indexOf(ask)
     var pos_accept = waiters_name.indexOf(accept)
 
-    socket.join(ask + "&" + accept)
-
     socket.to(waiters_id[pos_ask]).emit('startGame', ask, accept)
+
+    socket.join(ask + "&" + accept)
+      rooms.push(ask + "&" + accept)
+      console.log(rooms[0]+"=",socket.rooms)
+      io.to(ask + "&" + accept).emit('enterroom',ask + "&" + accept )
+
+
     // socket.to(waiters_id[pos_accept]).emit('startGame', ask, accept)
 
     console.log('Start game !' + ask + " and " + accept)
 
-    waiters_id = waiters_id.splice(pos_ask, 1)
-    waiters_id = waiters_id.splice(pos_accept, 1)
-    waiters_name = waiters_name.splice(pos_ask, 1)
-    waiters_name = waiters_name.splice(pos_accept, 1)
+    if(pos_ask<pos_accept){
+      waiters_id.splice(pos_accept, 1)
+      waiters_name.splice(pos_accept, 1)
+      waiters_id.splice(pos_ask, 1)
+      waiters_name.splice(pos_ask, 1)
+    }
+    else{
+      waiters_id.splice(pos_ask, 1)
+      waiters_name.splice(pos_ask, 1)
+      waiters_id.splice(pos_accept, 1)
+      waiters_name.splice(pos_accept, 1)
+    }
+
   })
 
   socket.on('disconnect', (data) => {
@@ -64,9 +81,22 @@ io.sockets.on('connection', (socket) => {
 
     if (waiters_id.includes(socket.id)) {
       var pos = waiters_id.indexOf(socket.id)
-      waiters_name = waiters_name.splice(pos, 1)
-      waiters_id = waiters_id.splice(pos, 1)
+      waiters_name.splice(pos, 1)
+      waiters_id.splice(pos, 1)
     }
+  })
+
+  socket.on('leave', (ask, accept, data)=>{
+    var ask = ask
+    var accept = accept
+    var waiter = data
+    socket.leave(ask + "&" + accept)
+    console.log(socket.rooms)
+    waiters_name.push(waiter)
+    waiters_id.push(socket.id)
+    console.log(waiters_name, waiters_id)
+
+
   })
 
 })
